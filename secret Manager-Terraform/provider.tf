c9b0d7fc84b9daca6a6c7d25607b82922909e906
Nishad Utpat <124704735+nishadutpat@users.noreply.github.com>
2025-02-13 18:58:18 +0530

    Update provider.tf

--- secret Manager-Terraform/provider.tf
+++ secret Manager-Terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "eu-north-1"
-  access_key = "AKIAXVPJGVNONCAAKLLM"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
